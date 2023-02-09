285049991dc798e573578eda62c2646f01296919
cwh12355 <cwh12355@163.com>
2023-02-09 17:13:07 +0800

    90/20/3202 [detail] learn loop and list for python

--- basic/do_for.py
+++ basic/do_for.py
@@ -4,5 +4,10 @@ for name in names:
     print (name)
 
 
+<<<<<<< HEAD
      qfor x in range(6):
         print()
+=======
+     for x in range(6)
+   print()
+>>>>>>> 6a0ec5f... 90/20/3202
--- basic/the_list.py
+++ basic/the_list.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 classmates = ['james','Baiden','Drump','Golf']
 print ('classmate = ',classmates )
 print('cleassmate[0] = ',classmates[0])
@@ -6,3 +7,6 @@ print('classmate[2] =',classmates[2])
 print('classmate[-1] =',classmates[-1])
 print(classmates)
 
+=======
+classsmates = ['']
+>>>>>>> 6a0ec5f... 90/20/3202
