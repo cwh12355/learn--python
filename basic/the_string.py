b2f2519edf02890a7476d9bf2769b2d71849cd27
cwh12355 <cwh12355@163.com>
2023-02-09 20:49:53 +0800

    tuple typeing no change

---
+++ basic/the_string.py
@@ -0,0 +1,14 @@
+s = 'python-韩文'
+print(s)
+s1 = s.encode('utf-8')
+print(s1)
+print(s1.decode('utf-8'))
+
+
+classmates = ('csss','dddd','aaaa')
+print('classmates[0] = ',classmates[0])
+
+
+classmates[0] = 'kbbb'
+print(classmates[0])
+
