583146956b32c49039a33c29b8cec52035d3619e
cwh12355 <cwh12355@163.com>
2023-02-09 17:13:07 +0800

    90/20/3202 [detail] learn loop and list for python

---
+++ basic/do_if.py
@@ -0,0 +1,11 @@
+
+age = int(input("input your age :"))
+if (age >=18):
+  print ("man or woman")
+elif (age <= 6):
+  print("child")
+elif (age >=7 & age <=18 ):
+  print("caowenhao")
+else :
+    print ("teenager")
+       
---
+++ basic/the_dict.py
@@ -0,0 +1,8 @@
+d = {
+    'Mac' : 23,
+    'Windows' : 4.4,
+    'Linux' : 100,
+}
+print('d[\'Mac\']= ',d['Mac'])
+print('d[\'Windows\'] = ',d['Windows'])
+print('d[\'Linux\'] = ',d['Linux'])
